2ddddc9c0a72b6a281de18893768447088699442
maciejimielowski <maciej.imielowski@gmail.com>
2016-09-01 19:44:41 +0200

    LastFm client - get friends and avatars

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,10 @@
+require_relative '../../lib/last_fm_client'
+
 class HomepageController < ApplicationController
+
 	def index
+		lastfm = LastFMClient.new
+		@friends = lastfm.lastfm.user.get_friends(:user => "genuyalnyul")
+		p @friends[0]["name"]
 	end
 end
