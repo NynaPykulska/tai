69b7dd13f45e1201ab3f110fb27df0a3f9af9145
Nina Pikulska <Nina Pikulska>
2016-09-04 16:37:57 +0200

    Comment modals layout

---
+++ lib/API_Keys.rb
@@ -0,0 +1,18 @@
+class API_Keys
+	@key
+	@secret
+
+	def initialize
+		@key = "426dda789364f83ca2d606d558e3f17a"
+		@secret = "6ffa6e71bbafc79bf098d3e46546ee4d"
+	end
+
+	def key
+		return @key
+	end
+
+	def secret
+		return @secret
+	end
+	
+end
